5f6f1e67d0a625200d47c5325479ab633ed38f1a
Robnibourg93 <robnibourg@hotmail.nl>
2016-04-02 15:31:27 +0200

    connection update
    connection update

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-
 var players = [];
 var bulletList = [];
 
-
-
-var io = require("socket.io").listen(server);
+var io = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
 
 io.on("connection", function (socket) {
 
